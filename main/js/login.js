440e4dc9841956fff82e943a8a21686ba6111f93
ejaygurl <114744504+ejaygurl@users.noreply.github.com>
2025-10-28 19:35:13 +0800

    Update login.js

--- main/js/login.js
+++ main/js/login.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
-
+//lagay sa env file
 const firebaseConfig = {
   apiKey: "AIzaSyB7gwN-3GVlJ-a_MTBxqVnc7Oltq5wSvHQ",
   authDomain: "acadbird-379e3.firebaseapp.com",
